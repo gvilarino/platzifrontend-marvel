4484e2143e231631186efd6276af731630a755f2
slifszyc <sacha.lifszyc@gmail.com>
2015-06-11 17:17:12 -0300

    Starting with io

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ app.use('*', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, function (err) {
+var server = require('http').Server(app);
+
+server.listen(port, function (err) {
   if (err) return console.log(err);
   console.log('App started on port ' + port);
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  socket.on('login', function (data) {
+    io.emit('login', { user: data });
+  });
 });
