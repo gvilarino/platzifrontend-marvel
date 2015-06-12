f12443c6e709fe950e0f3accbd74e58e2bb5f03e
Jose Fresco <josemariafresco@gmail.com>
2015-06-12 16:01:33 -0300

    Added LiveReload

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var uglify = require('gulp-uglify')
 var watchify = require('watchify')
 var assign = require('lodash.assign')
 
+var livereload = require('gulp-livereload')
 
 var opts = {
     entries: './lib/app.js', //punto de entrada js
@@ -35,27 +36,44 @@ gulp.task('js', function() {
   // .pipe(gulp.dest('./public/')) // en dónde va a estar el archivo destino
 })
 
-gulp.task('styl', function() {
+function styl () {
   return gulp.src('./lib/app.styl') // entry point de styl
     .pipe(stylus({ use: nib() })) //inicializo stylus con nib como plugin
     .pipe(concat('app.css'))
     // .pipe(minify())
     .pipe(gulp.dest('./public/css'))
+}
+
+gulp.task('styl', function() {
+  return styl()
 })
 
+gulp.task('styl:livereload', function () {
+  return styl().pipe(livereload({ start: true }))
+})
 
-gulp.task('watch', function() {
+gulp.task('styl:watch', ['styl:livereload'], function () {
+  return gulp.watch(['./lib/app.styl','./lib/**/*.styl'], ['styl:livereload'])
+})
+
+gulp.task('js:watch', function() {
   var w = watchify(browserify(opts));
 
   w.on('update', function (file) {
     console.log('file modified: %s, rebuilding...', file);
-    var b = generateBundle(w)
+    var b = generateBundle(w).pipe(livereload()) // Here we don't pass the start: true parameter since we know that has been initialized in line 70
     console.log('rebuilt')
     return b
   })
 
-  return generateBundle(w);
+  return generateBundle(w)
+    .pipe(livereload({ start: true }))
+    // livereload is a singleton, but with the first invoking we have to pass
+    // the start:true parameter, just to ensure that the server starts. If it is
+    // already started, nothing happens.
 })
+
+gulp.task('watch', ['js:watch', 'styl:watch'])
 
 function generateBundle(b) {
   return b
